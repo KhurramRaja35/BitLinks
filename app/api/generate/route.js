bd39753d8402210e0e7052b23e22227028cebaad
KhurramRaja35 <khurrambashirraja@gmail.com>
2025-06-30 00:26:56 +0500

    Fix Api  errors

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -3,7 +3,7 @@ import clientPromise from "@/lib/mongodb"
 
 
 export async function POST(request) {
-
+try {
     const body = await request.json()
     // console.log(body)
     const client =await clientPromise;
@@ -27,4 +27,8 @@ export async function POST(request) {
     })
 
   return Response.json({success : true, error:false, message: 'URL generated succesfully' })
+   } catch (error) {
+        console.error("API Error:", error)
+        return Response.json({ success: false, error: true, message: 'Internal Server Error' }, { status: 500 })
+    }
 }
